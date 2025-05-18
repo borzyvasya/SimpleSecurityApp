3dc5d175c370d68cb2c1156c406f3886b775603f
scarabush_scarabush <55894795+borzyvasya@users.noreply.github.com>
2025-05-18 19:14:19 +0300

    Adding jwt features...

--- src/main/java/ru/me/springcourse/SimpleSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/me/springcourse/SimpleSecurityApp/config/SecurityConfig.java
@@ -2,13 +2,16 @@ package ru.me.springcourse.SimpleSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.me.springcourse.SimpleSecurityApp.services.PersonDetailsService;
 
 @EnableWebSecurity
@@ -16,16 +19,19 @@ import ru.me.springcourse.SimpleSecurityApp.services.PersonDetailsService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
+    private final JWTFilter jwtFilter;
 
     @Autowired
-    public SecurityConfig(PersonDetailsService personDetailsService) {
+    public SecurityConfig(PersonDetailsService personDetailsService, JWTFilter jwtFilter) {
         this.personDetailsService = personDetailsService;
+        this.jwtFilter = jwtFilter;
     }
 
     @Override
    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
@@ -33,7 +39,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/process_login")
                 .defaultSuccessUrl("/hello", true)
                 .failureUrl("/auth/login?error")
-                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login");
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/auth/login")
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -45,7 +55,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
-
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+       return super.authenticationManagerBean();
     }
 }
--- src/main/java/ru/me/springcourse/SimpleSecurityApp/controllers/AuthController.java
+++ src/main/java/ru/me/springcourse/SimpleSecurityApp/controllers/AuthController.java
@@ -1,28 +1,44 @@
 package ru.me.springcourse.SimpleSecurityApp.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.me.springcourse.SimpleSecurityApp.dto.AuthenticationDTO;
+import ru.me.springcourse.SimpleSecurityApp.dto.PersonDTO;
 import ru.me.springcourse.SimpleSecurityApp.models.Person;
+import ru.me.springcourse.SimpleSecurityApp.security.JWTUtil;
 import ru.me.springcourse.SimpleSecurityApp.services.RegistrationService;
 import ru.me.springcourse.SimpleSecurityApp.util.PersonValidator;
 
 import javax.validation.Valid;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final RegistrationService registrationService;
     private final PersonValidator personValidator;
+    private final JWTUtil jwtUtil;
+    private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(RegistrationService registrationService, PersonValidator personValidator) {
+    public AuthController(RegistrationService registrationService,
+                          PersonValidator personValidator,
+                          JWTUtil jwtUtil,
+                          ModelMapper modelMapper, AuthenticationManager authenticationManager) {
         this.registrationService = registrationService;
         this.personValidator = personValidator;
+        this.jwtUtil = jwtUtil;
+        this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
     }
 
     @GetMapping("/login")
@@ -35,13 +51,38 @@ public class AuthController {
         return "auth/registration"; }
 
     @PostMapping("/registration")
-    public String performRegistration(@ModelAttribute("person") @Valid Person person,
+    public String performRegistration(@RequestBody @Valid PersonDTO personDTO,
                                       BindingResult bindingResult) {
+        Person person = convertToPerson(personDTO);
         personValidator.validate(person, bindingResult);
-        if (bindingResult.hasErrors()) { return "auth/registration"; }
+        if (bindingResult.hasErrors()) {
+            return Map.of("message", "Ошибка!").toString();
+        }
 
         registrationService.register(person);
 
-        return "redirect:/auth/login";
+        String token = jwtUtil.generateToken(person.getUsername());
+        return Map.of("jwt-token", token).toString();
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> performLogin(@RequestBody AuthenticationDTO authenticationDTO) {
+        UsernamePasswordAuthenticationToken authInputToken =
+                new UsernamePasswordAuthenticationToken(authenticationDTO.getUsername(),
+                authenticationDTO.getPassword());
+
+       try {
+           authenticationManager.authenticate(authInputToken);
+       } catch (BadCredentialsException e) {
+           return Map.of("message", "Incorrect credentials!");
+       }
+
+       String token = jwtUtil.generateToken(authenticationDTO.getUsername());
+
+       return Map.of("jwt-token", token);
+    }
+
+    public Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
     }
 }
